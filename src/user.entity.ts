43d84392188fd807928ebf6adc792fc97d8bf623
rcottinet <romancottinet@gmail.com>
2026-01-22 01:21:52 +0100

    update

--- src/user.entity.ts
+++ src/user.entity.ts
@@ -1,8 +1,8 @@
 import { compose } from './helpers/compose';
 import { BaseEntity, Entity, PrimaryColumn } from 'typeorm';
-import { WithCreatedAt } from 'src/mixins/entity-with-created-at.mixin';
-import { WithUpdatedAt } from 'src/mixins/entity-with-updated-at.mixin';
-import { WithDeletedAt } from 'src/mixins/entity-with-deleted-at.mixin';
+import { WithCreatedAt } from 'src/mixins/with-created-at.mixin';
+import { WithUpdatedAt } from 'src/mixins/with-updated-at.mixin';
+import { WithDeletedAt } from 'src/mixins/with-deleted-at.mixin';
 
 @Entity()
 export class User extends compose(
